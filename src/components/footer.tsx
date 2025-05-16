cb807eb29e40e810637f8fac9cdbd92199941cd0
Moises2304 <ngovenemoises2@gmail.com>
2025-05-16 16:22:55 +0200

    fix:  project footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,21 @@
+   import { AiTwotoneMail } from "react-icons/ai";
+   import { AiFillLinkedin } from "react-icons/ai";
+   import { FaFacebook } from "react-icons/fa";
+   import { FaXTwitter } from "react-icons/fa6";
+
 export const Footer = () => {
   return (
-    <footer>
-      <div>
+    <footer className="bg-violet-400 text-sm text-white flex justify-between px-10 py-10">
+      <div >
         <p>&copy; The Spotidados, {new Date().getFullYear()}</p>
         <p>Termos de uso | Musica | Dados</p>
       </div>
-      <div></div>
+      <div className="flex gap-4 items-center">
+        <AiTwotoneMail  size="2em" color="white" />
+        <AiFillLinkedin size="2em" color="white"/>
+        <FaFacebook size="2em" color="white"/>
+        <FaXTwitter size="2em" color="white"/>
+        </div>
     </footer>
   );
 };
