261a2c8024f36612f51b9b63b797cec2f2392d4e
Afonso Relane <afonso.relane@gmail.com>
2025-05-16 15:08:27 +0200

    feat: add home-page

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <p>&copy; The Spotidados, {new Date().getFullYear()}</p>
+        <p>Termos de uso | Musica | Dados</p>
+      </div>
+      <div></div>
+    </footer>
+  );
+};
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,30 @@
+import { Footer } from "../components/footer";
+
 export const Home = () => {
   return (
     <>
-      <main></main>
+      <main
+        className="flex flex-col items-center gap-4 p-4 bg-cover bg-center h-screen"
+        style={{ backgroundImage: `url('/img/backgroud-img.jpg')` }}
+      >
+        <div className="w-[247px] h-[61px] rounded-[16px] bg-white">
+          <h1>Spotidados</h1>
+        </div>
+        <div>
+          <img src="/img/girl.png" alt="" />
+        </div>
+        <div className="text-white">
+          <p>Veja aqui as musicas</p>
+          <p>mais escutadas</p>
+          <p>ao redor do mundo</p>
+        </div>
+        <button>Login</button>
+        <section>
+          <div></div>
+          <div></div>
+        </section>
+      </main>
+      <Footer />
     </>
   );
 };
