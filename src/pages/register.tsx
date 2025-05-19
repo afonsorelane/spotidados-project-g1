bc0ae5f3aca0c5c7419f3ecf0168b3ae1480c8a9
Emilio Sumbane <emilio.sumbane@gmail.com>
2025-05-19 13:16:42 +0300

    feat SIGN UP STRUCTURE

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,25 @@
 export const Register = () => {
        return(
         <>
-        
+            <div>
+                <h1 className = "font-extrabold">Sign Up</h1>
+                <p >Already have an account?
+                    <a href="/login" target="_blank">Sigin</a></p>
+            </div>
+
+            <form action="">
+                <input type="text" placeholder="First Name" />
+                <input type="text" placeholder="Lasst Name" />
+                <input type="text" placeholder="Country" />
+                <input type="email" placeholder="Email or Phone" />
+                <input type="password" placeholder="Password" />
+            </form>
+
+            <div>
+                <button className="cursor-pointer">
+                    Create Account
+                </button>
+            </div>
         </>
     )
 }
