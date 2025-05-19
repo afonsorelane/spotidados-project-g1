64e0ad06b4b03cc32c6aaf234c8046a8441630e6
Moises2304 <ngovenemoises2@gmail.com>
2025-05-19 13:41:50 +0200

    updete: login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -61,7 +61,7 @@ export const Login = () => {
             <input
               className="w-full bg-transparent border border-gray-600 text-white rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-violet-500"
               type="text"
-              placeholder="exemplo@example.com ou +25884XXXXXXX"
+              placeholder="Email /Numero"
               value={identifier}
               onChange={(e) => setIdentifier(e.target.value)}
               required
