55cefaa198a48b3262c31c8d53352ac91a399c56
Afonso Relane <afonso.relane@gmail.com>
2025-05-19 14:53:21 +0200

    feat: create a page user details

---
+++ src/components/footer-user.tsx
@@ -0,0 +1,27 @@
+import { House, User, MagnifyingGlass } from "phosphor-react";
+import { useNavigate } from "react-router-dom";
+
+export const FooterUser = () => {
+  const navigate = useNavigate();
+
+  return (
+    <footer className="bg-purple-800  text-white flex justify-between">
+      <button
+        className="flex-col items-center gap-1"
+        onClick={() => navigate("/userHistoric")}
+      >
+        <House size={32} />
+        Início
+      </button>
+      <button onClick={() => navigate("/userData")}>
+        <User size={32} />
+        Casa
+      </button>
+
+      <button className="flex-col gap-1" onClick={() => navigate("/search")}>
+        <MagnifyingGlass size={32} />
+        Pesquisar
+      </button>
+    </footer>
+  );
+};
