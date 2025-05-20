1294e8eeaceb62d4c3c97038ea6027030b32abc0
Emilio Sumbane <emilio.sumbane@gmail.com>
2025-05-20 13:29:28 +0300

    update: sign up style finished

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,33 +1,52 @@
+import{
+    ArrowLeft,
+}from "phosphor-react";
+
 export const Register = () => {
        return(
         <>
-            <div className=
-            "px-10 py-20 min-h-screen bg-cover bg-center bg-radial-[at_20%_95%] from-purple-950 via-fuchsia-800 to-purple-950 text-white">
-                <div>
-                <h1 className = "font-bold text-2xl mt-18">Sign Up</h1>
-                <p className="font-normal text-lg mt-2">Already have an account?  
-                    <a href="/login" target="_blank" className="font-medium">
-                    Sign in</a></p>
-                </div>
+            <div className=" items-center justify-center min-h-screen bg-cover bg-center" style=
+                {{ backgroundImage: `url('/img/backgroud-img.jpg')` }}>
 
-                <form className="mt-8">
-                    <input className="w-full rounded-xl p-4 bg-transparent text-white border mt-4"
-                    type="text" placeholder="First Name" required/>
-                    <input className="w-full rounded-xl p-4 bg-transparent text-white border mt-4"
-                    type="text" placeholder="Last Name" />
-                    <input className="w-full rounded-xl p-4 bg-transparent text-white border mt-4"
-                    type="text" placeholder="Country" />
-                    <input className="w-full rounded-xl p-4 bg-transparent text-white border mt-4"
-                    type="email" placeholder="Email or Phone" />
-                    <input className="w-full rounded-xl p-4 bg-transparent text-white border mt-4"
-                    type="password" placeholder="Password" required/>
-                </form>
+                <div className=" rounded-md p-8 w-full max-w-md">
+                    
+                    <div><button className=" flex justify-self-start text-gray-100"><ArrowLeft /></button></div>
 
-                <div>
-                    <button className="w-full bg-gray-900 rounded-xl p-4 mt-4">
-                        Create Account
-                    </button>
+                    <div>
+                        <h1 className = "font-bold text-2xl mt-18 text-gray-100">Sign Up</h1>
+                        <p className="font-normal text-lg mt-4 text-gray-100">Already have an account?  
+                            <a href="/login" target="_blank" className="font-medium">
+                            Sign in</a>
+                        </p>
+                    </div>
+
+                    <form className="mt-8">
+                        <input className="w-full rounded-xl p-6 bg-transparent text-white border mt-5"
+                            type="text" placeholder="First Name" required/>
+                        <input className="w-full rounded-xl p-6 bg-transparent text-white border mt-5"
+                            type="text" placeholder="Last Name" />
+                        <input className="w-full rounded-xl p-6 bg-transparent text-white border mt-5"
+                            type="text" placeholder="Country" />
+                        <input className="w-full rounded-xl p-6 bg-transparent text-white border mt-5"
+                            type="email" placeholder="Email or Phone" />
+                        <input className="w-full rounded-xl p-6 bg-transparent text-gray-100 border mt-5"
+                            type="password" placeholder="Password" required/>
+                    </form>
+
+                    <div>
+                        {/* <button className="w-full bg-gray-900 rounded-xl p-6 mt-4 text-gray-100 text-xl "> */}
+                        <button className="w-full rounded-xl p-6 mt-4 text-gray-100 text-lg font-semibold bg-gradient-to-r from-indigo-600 via-purple-500 to-purple-600 hover:brightness-110 transition cursor-pointer">
+                            Create Account
+                        </button>
+                    </div>
+
+                    <div className="flex justify-center">
+                        <p className="text-gray-400 mt-4" >
+                            Terms of Use | Privacy Policy
+                        </p>
+                    </div>                    
                 </div>
+                
             </div>
         </>
     )
